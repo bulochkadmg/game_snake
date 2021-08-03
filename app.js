73aaf6b29af71c2873c54b878696886bce208eec
Oleg Paslavskiy <bulochka.dmg@mgail.com>
2021-08-03 07:27:47 +0300

    create game score and drawing the borders

--- app.js
+++ app.js
@@ -9,3 +9,15 @@ let canvas = document.getElementById('canvas'),
 let blockSize = 10,
     widthInBlocks = width / blockSize,
     heightInBlocks = height / blockSize;
+
+// Game score
+let score = 0;
+
+// drawing the borders
+let drawBorder = function() {
+    ctx.fillStyle = 'Gray';
+    ctx.fillRect(0, 0, width, blockSize);
+    ctx.fillRect(0, height - blockSize, width, blockSize);
+    ctx.fillRect(0, 0, blockSize, height);
+    ctx.fillRect(width - blockSize, 0, blockSize, height);
+};

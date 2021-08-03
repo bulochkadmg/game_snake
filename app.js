33b101615d35fadee4db37be25802a9d1ecf9705
Oleg Paslavskiy <bulochka.dmg@mgail.com>
2021-08-03 07:16:23 +0300

    create variables

---
+++ app.js
@@ -0,0 +1,8 @@
+// create variables
+let canvas = document.getElementById('canvas'),
+    ctx = canvas.getContext('2d'),
+    width = canvas.width,
+    height = canvas.height;
+
+
+// divide the canvas into blocks
